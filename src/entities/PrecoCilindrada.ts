83a7d78c935e88ad213627785f4b5ce9b2a88456
vanimarcos <stackvany@gmail.com>
2023-08-11 20:57:41 +0100

    added preco cilindrada

---
+++ src/entities/PrecoCilindrada.ts
@@ -0,0 +1,12 @@
+interface PrecoCilindrada {
+    id: Number,
+    nome: String,
+    lotacao: Number,
+    veiculo_categoria_id: Number,
+    premio_trimestral: Number,
+    premio_semestral:Number,
+    pemio_anual: Number,
+    cilindrada: Number,
+}
+
+export default PrecoCilindrada;
