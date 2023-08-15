99f9b4d9a04a5606f74598f08d29c38609974838
vanimarcos <vm@infoco.ao>
2023-08-15 17:25:34 +0100

    added  precocilindrada to the index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 import Router from 'express';
 import pessoaRoutes from './pessoaRoutes';
 import veiculoRoutes from './veiculoRoutes';
+import precoCilindradaRoutes from './precoCinindradaRoutes';
 
 const routes = Router();
 
 routes.use(pessoaRoutes);
 routes.use(veiculoRoutes);
-
+routes.use(precoCilindradaRoutes);
 
 export default routes;
